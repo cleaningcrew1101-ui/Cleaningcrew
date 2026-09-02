326add234eecf18d33ae124988b396c07334b006
cleaningcrew1101-ui <cleaningcrew1101@gmail.com>
2026-09-01 22:45:55 -0700

    Add files via upload

---
+++ schedule.js
@@ -0,0 +1,14 @@
+// Edit this file on GitHub to update your available booking times.
+// To remove a time: delete it from the list.
+// To close a whole day: delete that entire line.
+// To add a time: add it in quotes, separated by commas.
+
+var schedule = {
+  "Monday":    ["3:30 PM", "4:30 PM", "5:30 PM"],
+  "Tuesday":   ["3:30 PM", "4:30 PM", "5:30 PM"],
+  "Wednesday": ["3:30 PM", "4:30 PM", "5:30 PM"],
+  "Thursday":  ["3:30 PM", "4:30 PM", "5:30 PM"],
+  "Friday":    ["3:30 PM", "4:30 PM", "5:30 PM"],
+  "Saturday":  ["8:00 AM", "10:00 AM", "12:00 PM", "2:00 PM", "4:00 PM"],
+  "Sunday":    ["9:00 AM", "11:00 AM", "1:00 PM", "3:00 PM"]
+};
